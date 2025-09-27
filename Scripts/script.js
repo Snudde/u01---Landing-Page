50fdb0f18cad9c644e7d8b81355339dcf0d145f1
mail@mail.com <kallesalomonsson@gmail.com>
2025-09-27 15:08:54 +0200

    Enhance project layout and styling
    - Updated script.js to set background image for project cards based on img element.
    - Added new CSS classes for about section, technologies, and project cards to improve layout and styling.
    - Introduced new CSS variables for consistent theming and improved readability.
    - Adjusted footer styling to ensure it remains at the bottom of the page.

--- Scripts/script.js
+++ Scripts/script.js
@@ -5,3 +5,10 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 })
+
+const imageDiv = document.querySelector('.image');
+const img = imageDiv.querySelector('img');
+if (img) {
+  imageDiv.style.backgroundImage = `url('${img.src}')`;
+  img.style.display = 'none'; // Optionally hide the img element
+}
